d28de2d111c796e42635a25a0a9f4de156687478
hendrasetiawanbentley <74999581+hendrasetiawanbentley@users.noreply.github.com>
2020-11-26 23:29:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,33 @@ This is my first deployed app
 '''
 
 df = pd.read_csv('dashboardready1.csv')
+df = df.loc[df["Country Name"]!= "Equatorial Guinea"]
+df = df.loc[df["Country Name"]!= "Argentina"]
+df = df.loc[df["Country Name"]!= "Brazil"]
+df = df.loc[df["Country Name"]!= "Bulgaria"]
+df = df.loc[df["Country Name"]!= "Zimbabwe"]
+df = df.loc[df["Country Name"]!= "Angola"]
+df = df.loc[df["Country Name"]!= "Belarus"]
+df = df.loc[df["Country Name"]!= "Tajikistan"]
+df = df.loc[df["Country Name"]!= "Bahamas, The"]
+df = df.loc[df["Country Name"]!= "Mongolia"]
+df = df.loc[df["Country Name"]!= "Armenia"]
+df = df.loc[df["Country Name"]!= "Uruguay"]
+df = df.loc[df["Country Name"]!= "Romania"]
+df = df.loc[df["Country Name"]!= "Bolivia"]
+df = df.loc[df["Country Name"]!= "Trinidad and Tobago"]
+df = df.loc[df["Country Name"]!= "Azerbaijan"]
+df = df.loc[df["Country Name"]!= "Ukraine"]
+df = df.loc[df["Country Name"]!= 'Kyrgyz Republic']
+df = df.loc[df["Country Name"]!= 'Malawi']
+df = df.loc[df["Country Name"]!= 'Madagascar']
+df = df.loc[df["Country Name"]!= 'Colombia']
+dftimeseries=df.loc[(df['Indicator Name']=='Real interest rate (%)') & (df['Value'].isna())]
+x=dftimeseries["Country Name"]
+available_indicators = df['Indicator Name'].unique()
+dftimeseriesfinal = df[~df["Country Name"].isin(x)]
+available_country=dftimeseriesfinal["Country Name"].unique()
+
 
 
 
